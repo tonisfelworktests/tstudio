957492ced9c9581772cf0667513200af41f30f89
tonisfeltr <seintte@gmail.com>
2022-11-21 20:04:41 +0700

    create migrations

--- database/migrations/2022_11_21_123447_create_inner_region_table.php
+++ database/migrations/2022_11_21_123447_create_inner_region_table.php
@@ -13,13 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('inner_region', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string("name", 16);
+            $table->integer("federalRegionId")
+                ->nullable();
             $table->timestamps();
         });
     }
@@ -31,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('inner_region');
     }
 };
